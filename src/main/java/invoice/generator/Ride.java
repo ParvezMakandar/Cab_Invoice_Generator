2e001019eb93864574c0e553a00784682389a5a8
parvez050 <parvezhm050@gmail.com>
2022-02-22 01:33:43 +0530

    Cab Invoice Generator

---
+++ src/main/java/invoice/generator/Ride.java
@@ -0,0 +1,12 @@
+package invoice.generator;
+
+public class Ride {
+    public final double distance;
+    public final int time;
+
+    //@Description-Parameterize Constructor is created.
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
